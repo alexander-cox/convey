41418415f886a7e517d58c4a8100c4fa2b71aa96
Alexander Cox <alexander.b.cox@outlook.com>
2019-11-25 19:51:17 +0000

    hurrah! it kind of works

--- convey/index.js
+++ convey/index.js
@@ -2,35 +2,93 @@ const http = require("http");
 
 class Convey {
   constructor() {
+    this.__middlewareIndex = 0;
+    this.__request;
+    this.__response;
+    this.__pathQueues = {};
+    this.__intialQueue = [];
     this.__server = http.createServer((request, response) => {
-      response.send = response.end;
-      this.conveyRequestHandler(request, response);
+      this.__request = request;
+      this.__response = response;
+
+      //some parsing of the req/res as it comes in...
+      this.__response.send = function(data) {
+        response.write(data);
+        response.end();
+      };
+      this.__request.path = request.url;
+      this.__conveyRequestHandler();
     });
-    this.__useFunc;
-    this.__getFunc;
   }
-  conveyRequestHandler(request, response) {
-    if (this.__getFunc && this.__getPath) {
-      if (request.url === this.__getPath && request.method === "GET") {
-        this.__getFunc(request, response);
+
+  listen(PORT, callback) {
+    return this.__server.listen(PORT, callback);
+  }
+  __nextMiddlewareHandler() {
+    const { path, method } = this.__request;
+    const middleware = this.__pathQueues[path][method][this.__middlewareIndex];
+    if (middleware) {
+      middleware(this.__request, this.__response, this.__next.bind(this));
+    }
+  }
+  __next() {
+    this.__middlewareIndex++;
+    this.__nextMiddlewareHandler();
+  }
+  __conveyRequestHandler() {
+    //start the counter at 0 when a new request comes in
+    this.__middlewareIndex = 0;
+    this.__nextMiddlewareHandler();
+  }
+  __queueHttpMethodMiddleware(path, method, middleware) {
+    const { __pathQueues, __intialQueue } = this;
+    if (!__pathQueues[path]) {
+      __pathQueues[path] = { [method]: [].concat(__intialQueue) };
+    }
+    if (!__pathQueues[path][method]) {
+      __pathQueues[path][method] = [].concat(__intialQueue);
+    }
+    __pathQueues[path][method].push(middleware);
+  }
+  use(middleware) {
+    const { __intialQueue, __pathQueues } = this;
+    const hasPathQueues = Object.keys(__pathQueues).length > 0;
+    if (!hasPathQueues) __intialQueue.push(middleware);
+    else {
+      for (path in __pathQueues) {
+        for (method in __pathQueues[path]) {
+          __pathQueues[path][method].push(middleware);
+        }
       }
     }
-    if (this.__useFunc) this.__useFunc(request, response);
   }
-  listen(PORT, callback) {
-    this.__server.listen(PORT, callback);
+  get(path, middleware) {
+    this.__queueHttpMethodMiddleware(path, "GET", middleware);
   }
-  use(handler) {
-    this.__useFunc = handler;
+  post(path, middleware) {
+    this.__queueHttpMethodMiddleware(path, "POST", middleware);
   }
-  get(path, handler) {
-    this.__getPath = path;
-    this.__getFunc = handler;
+  patch(path, middleware) {
+    this.__queueHttpMethodMiddleware(path, "PATCH", middleware);
+  }
+  put(path, middleware) {
+    this.__queueHttpMethodMiddleware(path, "PUT", middleware);
   }
 }
 
 function convey() {
   return new Convey();
 }
+
+convey.bodyParser = function bodyParser(request, response, next) {
+  let bodyData = "";
+  request.on("data", chunk => {
+    bodyData += chunk.toString();
+  });
+  request.on("end", () => {
+    request.body = JSON.parse(bodyData);
+    next();
+  });
+};
 
 module.exports = convey;
--- testing-convey.js
+++ testing-convey.js
@@ -2,12 +2,21 @@ const convey = require("./convey");
 
 const app = convey();
 
-app.use((req, res) => {
-  res.send("booo!");
-});
+// app.use((req, res) => {
+//   console.log(res);
+//   res.send("booo!");
+// });
+
+app.use(convey.bodyParser);
 
 app.get("/hello", (req, res) => {
   res.send("well hello there!");
+});
+
+app.post("/hello", function(req, res) {
+  const body = req.body;
+  const stringyBody = JSON.stringify({ body });
+  res.send(stringyBody);
 });
 
 app.listen(8080, () => {
